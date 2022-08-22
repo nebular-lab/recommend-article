2ba356d31fc4a45b11d0608c56f47e1751844f3b
nebular-lab <hiranodaisuke0721@gmail.com>
2022-08-23 00:22:34 +0900

    カウント機能の追加

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -1,17 +1,29 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export default function Post({ postData }) {
+  const [counts, setCounts] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const onClickAdd = (recommendID) => {
+    const _counts = [...counts];
+    _counts[recommendID - 1]++;
+    setCounts(_counts);
+  };
   return (
     <div>
       <div>{postData.contents}</div>
       {postData.recommendIDs.map((recommendID) => {
         return (
           <Link key={recommendID} href={`/post/${recommendID}`}>
-            <button>{`記事${recommendID}`}</button>
+            <button onClick={() => onClickAdd(recommendID)}>{`記事${recommendID}`}</button>
           </Link>
         );
       })}
+      <div>
+        {counts.map((count, index) => {
+          return <div key={index}>{count}</div>;
+        })}
+      </div>
     </div>
   );
 }
